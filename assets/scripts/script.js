de85aa3f5c7c96a1e80d8976cf9d8c3ab5cb9519
“Leonssizm” <levani.kereselidze11@gmail.com>
2022-06-08 12:27:29 +0400

    added some features in javascript

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,8 +1,7 @@
 
-
 function openAddForm() {
-    let addModalBackdrop = document.querySelector('#add-modal-backdrop')
-    addModalBackdrop.style.display = "flex"
+    let addModalBackdrop = document.querySelector('#add-modal-backdrop');
+    addModalBackdrop.style.display = "flex";
 
 }
 function closeAddForm() {
@@ -11,12 +10,49 @@ function closeAddForm() {
 }
 
 function addBeast() {
-    const nameInput = document.querySelector ('#name-input').value
+    const nameInput = document.querySelector('#name-input').value;
 
-    let newElement = document.createElement('li');
-    newElement.classList.add('list-group-item')
-    newElement.textContent = nameInput;
-    
-    const contactsList = document.querySelector('#contacts')
-    contactsList.appendChild(newElement)
+    let listTemplate = document.querySelector('#list-template');
+    let clonedList = listTemplate.content.cloneNode(true);
+    clonedList.querySelector('li').innerText = nameInput;
+
+    let contactsDisplay = document.querySelector('#contacts');
+    contactsDisplay.appendChild(clonedList);
 }
+function closeCard() {
+    let closeCard = document.querySelector('#cards');
+    closeCard.style.display = "none";
+    console.dir(closeCard)
+    
+}
+
+function appendTemplateToBody(){
+    let template = document.querySelector('#card-template');
+    let clonedCard = template.content.cloneNode(true);
+    
+
+    let cardsDisplay = document.querySelector('#card-display');
+    cardsDisplay.appendChild(clonedCard);
+}
+
+function deleteCard(){
+    closeCard();
+    let clonedList = document.querySelector("li");
+    let clonedListElement = clonedList;
+    clonedList.style.display = "none";
+}
+
+// modifying the name used in the cards according to the name that user provides in the user input.
+
+/* under construction
+function modifyCardName () {
+
+    
+    let nameInput = document.querySelector('#name-input').value;
+    let givenName = document.querySelector("card-name");
+    
+
+}
+*/ 
+
+
