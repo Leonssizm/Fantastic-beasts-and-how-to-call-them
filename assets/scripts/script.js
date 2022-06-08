b95fd8add7d096476c6449e99f94393b5ad1628b
“Leonssizm” <levani.kereselidze11@gmail.com>
2022-06-09 00:00:54 +0400

    improvments to Javascript

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,58 +1,124 @@
 
+let beasts = [
+    { name: "fox", phone: "598 999 999" },
+    { name: "bear", phone: "598 111 222" }
+];
+
+
+function firstVisit(nme) {
+    for (let i = 0; i < beasts.length; i++) {
+
+        // prepare element
+        let listTemplate = document.querySelector('#list-template');
+        let clonedList = listTemplate.content.cloneNode(true);
+        clonedList.querySelector('li').innerText = beasts[i].name;
+        clonedList.querySelector('li').id = "li-" + beasts[i].name;
+
+        // append element to DOM
+        let contactsDisplay = document.querySelector('#contacts');
+        contactsDisplay.appendChild(clonedList);
+    }
+}
+
+firstVisit();
+
+
 function openAddForm() {
     let addModalBackdrop = document.querySelector('#add-modal-backdrop');
     addModalBackdrop.style.display = "flex";
-
 }
+
 function closeAddForm() {
     let addModalBackdrop = document.querySelector('#add-modal-backdrop')
     addModalBackdrop.style.display = "none"
 }
 
-function addBeast() {
-    const nameInput = document.querySelector('#name-input').value;
 
+
+function addBeast() {
+    // get input values
+    const name = document.querySelector('#name-input').value;
+    const phone = document.querySelector('#number-input').value;
+
+    // add to beasts array
+    const newBeast = {
+        name: name,
+        phone: phone,
+    }
+    beasts.push(newBeast);
+
+    // prepare element
     let listTemplate = document.querySelector('#list-template');
     let clonedList = listTemplate.content.cloneNode(true);
-    clonedList.querySelector('li').innerText = nameInput;
+    clonedList.querySelector('li').innerText = name;
+    clonedList.querySelector('li').id = 'li-' + name;
 
+    // append element to DOM
     let contactsDisplay = document.querySelector('#contacts');
     contactsDisplay.appendChild(clonedList);
 }
-function closeCard() {
-    let closeCard = document.querySelector('#cards');
-    closeCard.style.display = "none";
-    console.dir(closeCard)
-    
-}
 
-function appendTemplateToBody(){
+
+
+
+
+function addCard(el) {
+    // get beast id
+    const beastId = el.id.substring(3);
+
+    // TASK
+    // BUG FIX:
+    // check if car with that id already exists
+    // if card does not exist with id card-{beastId} and do everything we are already doing
+    // else do nothing
+
+
+
+
+
+
+    // get beast data from array
+    let beast;
+    for (let i = 0; i < beasts.length; i++) {
+        if (beasts[i].name === beastId) {
+            beast = beasts[i];
+        }
+    }
+
+    // clone card template
     let template = document.querySelector('#card-template');
     let clonedCard = template.content.cloneNode(true);
-    
 
+    // change name & phone
+    clonedCard.querySelector('h2').innerText = beast.name;
+    clonedCard.querySelector('p').innerText = beast.phone;
+
+    // display card
     let cardsDisplay = document.querySelector('#card-display');
     cardsDisplay.appendChild(clonedCard);
 }
 
-function deleteCard(){
-    closeCard();
-    let clonedList = document.querySelector("li");
-    let clonedListElement = clonedList;
-    clonedList.style.display = "none";
-}
 
-// modifying the name used in the cards according to the name that user provides in the user input.
+// TASK:
+function closeCard(closeButton) {
+    // Delete from DOM
 
-/* under construction
-function modifyCardName () {
 
-    
-    let nameInput = document.querySelector('#name-input').value;
-    let givenName = document.querySelector("card-name");
-    
+    // Remove from beasts array
+    let closeCard = document.querySelector('#cards');
+    closeCard.style.display = "none";
+    console.dir(closeCard)
 
 }
-*/ 
 
+// TASK:
+function deleteCard() {
+    // do same as closeCard
 
+    // also remove li element from sidebar from DOM
+}
+
+// TASK
+function clallBeast() {
+
+}
